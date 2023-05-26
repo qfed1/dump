87d2319ab03cc4f06f9667fef8b2c09d28ed5432
evan badding <evanbadding@gmail.com>
2023-05-26 17:20:27 -0600

    le

--- lean.py
+++ lean.py
@@ -2,10 +2,13 @@ from keys import token
 import logging
 import sqlite3
 import asyncio
-from telegram import __version__ as TG_VER
+#from telegram import __version__ as TG_VER
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
+import pkg_resources
 from collections import defaultdict
+
+TG_VER = pkg_resources.get_distribution("telegram").version
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
