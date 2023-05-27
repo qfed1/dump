ee27f5ac9353ab504d57fdb91d56223a8d0c6c6c
evan badding <evanbadding@gmail.com>
2023-05-26 21:44:25 -0600

    cool1

--- leanfancy.py
+++ leanfancy.py
@@ -2,7 +2,6 @@ from keys import token
 import logging
 import sqlite3
 import asyncio
-import re
 from telegram import __version__ as TG_VER
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
@@ -54,16 +53,95 @@ async def alarm(context: ContextTypes.DEFAULT_TYPE) -> None:
     eth_address, message_text = row if row else ("", "")
     message_text = message_text.replace("Make sure to join our Alpha Community: @NovelApes so we can make bank together during the next bulla!", "")
 
-    # Extract Ethereum address between "Etherscan The Address" and "Comment" and remove from message_text
-    match = re.search(r"Etherscan The Address(.*?)Comment", message_text)
-    if match:
-        extracted_address = match.group(1).strip()
-        message_text = message_text.replace(match.group(0), "")
+    start = message_text.find("Etherscan The Address") + len("Etherscan The Address")
+    end = message_text.find("Comment")
+    eth_address = message_text[start:end].strip()
 
-        # Add Etherscan link with extracted address
-        links_text += f"\nEtherscan: https://etherscan.io/token/{extracted_address}"
+    message_text = message_text[:start] + message_text[end:]
+    eth_address_link = f"https://etherscan.io/tokens/{eth_address}"
 
-    # Rest of the code...
+    # Create the links with the fetched eth_address_link
+    links_text = "\n\n".join([
+        f"Honeypot: https://honeypot.is/ethereum.html?address={eth_address_link}",
+        f"Tokensniffer: https://tokensniffer.com/token/{eth_address_link}",
+        f"Dextools: https://www.dextools.io/app/ether/pair-explorer/{eth_address_link}",
+        f"Dexscreener: https://dexscreener.com/ethereum/{eth_address_link}",
+        f"coinscan: https://www.coinscan.com/tokens/{eth_address_link}",
+        f"Holders: https://etherscan.io/token/{eth_address_link}#balances",
+        f"Owner: https://etherscan.io/address/{eth_address_link}",
+        f"Contract: https://etherscan.io/token/{eth_address_link}",
+        f"Uniswap: https://app.uniswap.org/#/swap?outputCurrency={eth_address_link}",
+        f"1inch: https://app.1inch.io/#/1/unified/swap/ETH/{eth_address_link}",
+    ])
+
+    # Remove all text after "Scanners: Honeypot" and replace "Scanners: Honeypot" with "Scanners: "
+    scanner_index = message_text.find("Scanners: Honeypot")
+    if scanner_index != -1:
+        message_text = message_text[:scanner_index] + "Scanners: "
+
+    # Split the message text into lines at each "|" character
+    message_text = "\n\n".join(message_text.split("|"))
+
+    # Create the final message
+    message = f"Row {timer_beep_counter}: {eth_address_link}\n\n{message_text}\n\n{links_text}"
+
+    try:
+        if len(message) <= MAX_MESSAGE_LENGTH:
+            await context.bot.send_message(job.chat_id, text=message)
+        else:
+            messages = [message[i:i+MAX_MESSAGE_LENGTH] for i in range(0, len(message), MAX_MESSAGE_LENGTH)]
+            for i, msg in enumerate(messages, 1):
+                await context.bot.send_message(job.chat_id, text=f"{msg}\n\nPart {i}/{len(messages)}")
+    except telegram.error.RetryAfter as e:
+        await asyncio.sleep(e.retry_after)  # Wait for the specified time before retrying
+        job_warnings[job.name] += 1  # Increment the warning count for this job
+    else:
+        job_warnings[job.name] = 0  # Reset the count if we successfully sent a message
+
+    timer_beep_counter += 1
+    print(f"Beep! {job.data} seconds are over! This is beep number {timer_beep_counter}.")
+
+def remove_job_if_exists(name: str, context: ContextTypes.DEFAULT_TYPE) -> bool:
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+async def set_timer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    chat_id = update.effective_message.chat_id
+    try:
+        due = float(context.args[0])
+        if due < 0:
+            await update.effective_message.reply_text("Sorry we can not go back to future!")
+            return
+
+        job_removed = remove_job_if_exists(str(chat_id), context)
+        context.job_queue.run_repeating(alarm, due, chat_id=chat_id, name=str(chat_id), data=due)
+
+        text = "Timer successfully set!"
+        if job_removed:
+            text += " Old one was removed."
+        await update.effective_message.reply_text(text)
+
+    except (IndexError, ValueError):
+        await update.effective_message.reply_text("Usage: /set <seconds> DO NOT SET LOWER THAN 0.43 SECONDS UNDER NO CIRCUMSTANCES!!!")
+
+async def unset(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = "Timer successfully cancelled!" if job_removed else "You have no active timer."
+    await update.message.reply_text(text)
+
+def main() -> None:
+    application = Application.builder().token(token).build()
+
+    application.add_handler(CommandHandler(["start", "help"], start))
+    application.add_handler(CommandHandler("set", set_timer))
+    application.add_handler(CommandHandler("unset", unset))
+
+    application.run_polling()
 
 if __name__ == "__main__":
     main()
