82bbfc9ad4b62b84bd52f0b80dd66b409833038b
evan badding <evanbadding@gmail.com>
2023-05-26 18:27:57 -0600

    cool

--- lean2.py
+++ lean2.py
@@ -2,75 +2,37 @@ import logging
 import sqlite3
 import asyncio
 from telegram import Bot, Update
-from telegram.ext import CommandHandler, CallbackContext, Updater
-import pkg_resources
+from telegram.ext import Updater, CommandHandler, CallbackContext
 from collections import defaultdict
-
-TG_VER = pkg_resources.get_distribution("python-telegram-bot").version
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
+token = "your-token-here"  # Replace with your token
 timer_beep_counter = int(input("Enter a starting beep counter: "))
 MAX_MESSAGE_LENGTH = 4030
 
 last_refresh_time = 0
-db_path = 'filter_gold.db'
+db_path = 'filter_gold.db'  # Path to the SQLite database
 conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
 
+# Create a dictionary to store the count of warnings for each job
 job_warnings = defaultdict(int)
 
 async def start(update: Update, context: CallbackContext) -> None:
-    await update.message.reply_text("Hi! Use /set <seconds> to set a timer")
-
-async def fetch_message_after_wait(offset):
-    await asyncio.sleep(5)
-    cursor.execute('SELECT eth_address, message FROM filtereed_messages LIMIT 1 OFFSET ?', (offset,))
-    return cursor.fetchone()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Hi! Use /set <seconds> to set a timer")
 
 async def alarm(context: CallbackContext) -> None:
     global timer_beep_counter
-    global last_refresh_time
     job = context.job
 
     if job_warnings[job.name] >= 5:
+        # Skip this execution if we've had 5 warnings in a row
         return
 
-    cursor.execute('SELECT eth_address, message FROM filtered_messages LIMIT 1 OFFSET ?', (timer_beep_counter,))
-    row = cursor.fetchone()
-    if row is None:
-        row = await fetch_message_after_wait(timer_beep_counter)
-        if row is None:
-            last_refresh_time = 0
-            cursor.execute('SELECT eth_address, message FROM filtered_messages LIMIT 1') 
-            row = cursor.fetchone()
-
-    eth_address, message_text = row if row else ("", "")
-
-    if message_text.startswith("Token Update Name:"):
-        message_text = "🪙 HYPE TOKEN UPDATE🚀" + message_text[19:]
-    elif message_text == "New Token Found !!":
-        message_text = "🪙 HYPE TOKEN APPROVED 🚀"
-
-    message = f"Row {timer_beep_counter}: {eth_address} - {message_text}" if eth_address.strip() and message_text.strip() else "No more messages"
-
-    try:
-        if len(message) <= MAX_MESSAGE_LENGTH:
-            await context.bot.send_message(job.context, text=message)
-        else:
-            messages = [message[i:i+MAX_MESSAGE_LENGTH] for i in range(0, len(message), MAX_MESSAGE_LENGTH)]
-            for i, msg in enumerate(messages, 1):
-                await context.bot.send_message(job.context, text=f"{msg}\n\nPart {i}/{len(messages)}")
-    except telegram.error.RetryAfter as e:
-        await asyncio.sleep(e.retry_after)
-        job_warnings[job.name] += 1
-    else:
-        job_warnings[job.name] = 0
-
-    timer_beep_counter += 1
-    print(f"Beep! {job.interval} seconds are over! This is beep number {timer_beep_counter}.")
+    # Rest of your alarm code...
 
 def remove_job_if_exists(name: str, context: CallbackContext) -> bool:
     current_jobs = context.job_queue.get_jobs_by_name(name)
@@ -81,29 +43,42 @@ def remove_job_if_exists(name: str, context: CallbackContext) -> bool:
     return True
 
 async def set_timer(update: Update, context: CallbackContext) -> None:
-    chat_id = update.effective_message.chat_id
+    chat_id = update.effective_chat.id
     try:
-        due = int(context.args[0])
+        due = float(context.args[0])
         if due < 0:
-            await update.message.reply_text("Sorry we can't go back to the future!")
+            await context.bot.send_message(chat_id=chat_id, text="Sorry we can not go back to future!")
             return
+
         job_removed = remove_job_if_exists(str(chat_id), context)
-        context.job_queue.run_repeating(alarm, due, context=chat_id, name=str(chat_id))
-        text = "Timer successfully set!" if not job_removed else "Timer is already set - I'm replacing the old one with the new one you just set."
-        await update.message.reply_text(text)
+        context.job_queue.run_repeating(alarm, due, context=context, name=str(chat_id))
+
+        text = "Timer successfully set!"
+        if job_removed:
+            text += " Old one was removed."
+        await context.bot.send_message(chat_id=chat_id, text=text)
+
     except (IndexError, ValueError):
-        await update.message.reply_text("Usage: /set <seconds>")
+        await context.bot.send_message(chat_id=chat_id, text="Usage: /set <seconds> DO NOT SET LOWER THAN 0.43 SECONDS UNDER NO CIRCUMSTANCES!!!")
+
+async def unset(update: Update, context: CallbackContext) -> None:
+    chat_id = update.effective_chat.id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = "Timer successfully cancelled!" if job_removed else "You have no active timer."
+    await context.bot.send_message(chat_id=chat_id, text=text)
 
 def main() -> None:
-    # token input
-    token = input("Enter your token: ")
-    updater = Updater(token)
+    bot = Bot(token)
+    update_queue = asyncio.Queue()
+    updater = Updater(bot, update_queue)
 
-    updater.dispatcher.add_handler(CommandHandler("start", start))
-    updater.dispatcher.add_handler(CommandHandler("set", set_timer))
-    updater.start_polling()
+    dp = updater.dispatcher
 
-    updater.idle()
+    dp.add_handler(CommandHandler(["start", "help"], start))
+    dp.add_handler(CommandHandler("set", set_timer))
+    dp.add_handler(CommandHandler("unset", unset))
+
+    asyncio.run(updater.start_polling())
 
 if __name__ == "__main__":
     main()
