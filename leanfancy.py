52717148bf3d7d39383cd4ffe22bd5ce95c1530b
evan badding <evanbadding@gmail.com>
2023-05-27 20:16:44 -0600

    ok

--- leanfancy.py
+++ leanfancy.py
@@ -4,11 +4,8 @@ import sqlite3
 import asyncio
 import re
 import os
-import subprocess
-from telegram import __version__ as TG_VER
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
-from collections import defaultdict
+from telegram import Update, __version__ as TG_VER
+from telegram.ext import CommandHandler, CallbackContext, Updater
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -17,30 +14,27 @@ logging.basicConfig(
 timer_beep_counter = int(input("Enter a starting beep counter: "))
 MAX_MESSAGE_LENGTH = 4030
 
-last_refresh_time = 0
 db_path = 'filter_gold.db'  # Path to the SQLite database
-archive_db_path = 'filter_gold_archive.db'  # Path to the archive SQLite database
-
+db_path_archive = 'filter_gold_archive.db'  # Path to the archive SQLite database
 conn = sqlite3.connect(db_path)
+conn_archive = sqlite3.connect(db_path_archive)
 cursor = conn.cursor()
-
-conn_archive = sqlite3.connect(archive_db_path)
 cursor_archive = conn_archive.cursor()
 
 # Create a dictionary to store the count of warnings for each job
 job_warnings = defaultdict(int)
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Hi! Use /set <seconds> to set a timer")
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text("Hi! Use /set <seconds> to set a timer")
 
-async def fetch_message_after_wait(offset):
-    await asyncio.sleep(5)
-    cursor.execute('SELECT eth_address, message FROM filtereed_messages LIMIT 1 OFFSET ?', (offset,))
+def fetch_message_after_wait(offset):
+    asyncio.sleep(5)
+    cursor.execute('SELECT eth_address, message FROM filtered_messages LIMIT 1 OFFSET ?', (offset,))
     return cursor.fetchone()
 
 import telegram.error
 
-async def alarm(context: ContextTypes.DEFAULT_TYPE) -> None:
+def alarm(context: CallbackContext) -> None:
     global timer_beep_counter
     global last_refresh_time
     job = context.job
@@ -52,14 +46,13 @@ async def alarm(context: ContextTypes.DEFAULT_TYPE) -> None:
     cursor.execute('SELECT eth_address, message FROM filtered_messages LIMIT 1 OFFSET ?', (timer_beep_counter,))
     row = cursor.fetchone()
     if row is None:
-        row = await fetch_message_after_wait(timer_beep_counter)
+        row = fetch_message_after_wait(timer_beep_counter)
         if row is None:
-            main()
+            last_refresh_time = 0
+            cursor.execute('SELECT eth_address, message FROM filtered_messages LIMIT 1') 
+            row = cursor.fetchone()
 
     eth_address, message_text = row if row else ("", "")
-    if check_in_archive(eth_address, message_text):
-        return
-
     message_text = message_text.replace("Make sure to join our Alpha Community: @NovelApes so we can make bank together during the next bulla!", "")
 
     start = message_text.find("Etherscan The Address") + len("Etherscan The Address")
@@ -100,23 +93,29 @@ async def alarm(context: ContextTypes.DEFAULT_TYPE) -> None:
     # Create the final message
     message = f"Row {timer_beep_counter}: {eth_address_link}\n\n{message_text}\n\n{links_text}"
 
-    try:
-        if len(message) <= MAX_MESSAGE_LENGTH:
-            await context.bot.send_message(job.chat_id, text=message)
+    cursor_archive.execute("SELECT * FROM filtered_messages WHERE eth_address = ? AND message = ?", (eth_address, message_text))
+    archive_data = cursor_archive.fetchone()
+
+    if not archive_data:
+        cursor_archive.execute("INSERT INTO filtered_messages (eth_address, message) VALUES (?, ?)", (eth_address, message_text))
+
+        try:
+            if len(message) <= MAX_MESSAGE_LENGTH:
+                context.bot.send_message(job.chat_id, text=message)
+            else:
+                messages = [message[i:i+MAX_MESSAGE_LENGTH] for i in range(0, len(message), MAX_MESSAGE_LENGTH)]
+                for i, msg in enumerate(messages, 1):
+                    context.bot.send_message(job.chat_id, text=f"{msg}\n\nPart {i}/{len(messages)}")
+        except telegram.error.RetryAfter as e:
+            asyncio.sleep(e.retry_after)  # Wait for the specified time before retrying
+            job_warnings[job.name] += 1  # Increment the warning count for this job
         else:
-            messages = [message[i:i+MAX_MESSAGE_LENGTH] for i in range(0, len(message), MAX_MESSAGE_LENGTH)]
-            for i, msg in enumerate(messages, 1):
-                await context.bot.send_message(job.chat_id, text=f"{msg}\n\nPart {i}/{len(messages)}")
-    except telegram.error.RetryAfter as e:
-        await asyncio.sleep(e.retry_after)  # Wait for the specified time before retrying
-        job_warnings[job.name] += 1  # Increment the warning count for this job
-    else:
-        job_warnings[job.name] = 0  # Reset the count if we successfully sent a message
+            job_warnings[job.name] = 0  # Reset the count if we successfully sent a message
 
     timer_beep_counter += 1
     print(f"Beep! {job.data} seconds are over! This is beep number {timer_beep_counter}.")
 
-def remove_job_if_exists(name: str, context: ContextTypes.DEFAULT_TYPE) -> bool:
+def remove_job_if_exists(name: str, context: CallbackContext) -> bool:
     current_jobs = context.job_queue.get_jobs_by_name(name)
     if not current_jobs:
         return False
@@ -124,12 +123,12 @@ def remove_job_if_exists(name: str, context: ContextTypes.DEFAULT_TYPE) -> bool:
         job.schedule_removal()
     return True
 
-async def set_timer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+def set_timer(update: Update, context: CallbackContext) -> None:
     chat_id = update.effective_message.chat_id
     try:
         due = float(context.args[0])
         if due < 0:
-            await update.effective_message.reply_text("Sorry we can not go back to future!")
+            update.effective_message.reply_text("Sorry we can not go back to future!")
             return
 
         job_removed = remove_job_if_exists(str(chat_id), context)
@@ -138,26 +137,16 @@ async def set_timer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         text = "Timer successfully set!"
         if job_removed:
             text += " Old one was removed."
-        await update.effective_message.reply_text(text)
+        update.effective_message.reply_text(text)
 
     except (IndexError, ValueError):
-        await update.effective_message.reply_text("Usage: /set <seconds> DO NOT SET LOWER THAN 0.43 SECONDS UNDER NO CIRCUMSTANCES!!!")
+        update.effective_message.reply_text("Usage: /set <seconds> DO NOT SET LOWER THAN 0.43 SECONDS UNDER NO CIRCUMSTANCES!!!")
 
-async def unset(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+def unset(update: Update, context: CallbackContext) -> None:
     chat_id = update.message.chat_id
     job_removed = remove_job_if_exists(str(chat_id), context)
     text = "Timer successfully cancelled!" if job_removed else "You have no active timer."
-    await update.message.reply_text(text)
-
-def check_in_archive(eth_address, message):
-    cursor_archive.execute('SELECT * FROM filtered_messages WHERE eth_address = ? AND message = ?', (eth_address, message))
-    row = cursor_archive.fetchone()
-    if row is not None:
-        return True
-    else:
-        cursor_archive.execute('INSERT INTO filtered_messages (eth_address, message) VALUES (?, ?)', (eth_address, message))
-        conn_archive.commit()
-        return False
+    update.message.reply_text(text)
 
 def main():
     if os.path.exists(db_path):
@@ -166,15 +155,13 @@ def main():
     os.system("python3 filter.py")
     os.system("python3 newfilter.py")
 
-    Application(
-        token=token,
-        handlers=[
-            CommandHandler("start", start),
-            CommandHandler("set", set_timer),
-            CommandHandler("unset", unset),
-        ],
-        context_types=ContextTypes(DEFAULT_TYPE=asyncio.run),
-    ).start()
+    updater = Updater(token=token)
+    updater.add_handler(CommandHandler("start", start))
+    updater.add_handler(CommandHandler("set", set_timer))
+    updater.add_handler(CommandHandler("unset", unset))
+
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == '__main__':
     main()
