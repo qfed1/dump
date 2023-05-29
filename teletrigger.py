d44def7aecd557d07446461b5f775351d55834e2
evan badding <evanbadding@gmail.com>
2023-05-29 14:09:37 -0600

    sql

--- teletrigger.py
+++ teletrigger.py
@@ -1,9 +1,11 @@
 import asyncio
 import aiosqlite
 from telegram.ext import ApplicationBuilder
+from keys import token
+from keys import chatidmaster
 
-bot_token = "<bot-token>"
-chat_id = "<chat-id>"
+bot_token = token
+chat_id = chatidmaster
 
 # using ApplicationBuilder
 async def send_more(chat, msg):
@@ -11,10 +13,10 @@ async def send_more(chat, msg):
     await application.bot.sendMessage(chat_id=chat, text=msg)
 
 # Function to retrieve rows from database
-async def fetch_rows_from_db(db_path, last_row_id=0):
+async def fetch_rows_from_db(db_path, offset=0):
     db = await aiosqlite.connect(db_path)
     cursor = await db.cursor()
-    await cursor.execute("SELECT * FROM gold WHERE rowid > ?", (last_row_id,))
+    await cursor.execute("SELECT * FROM gold LIMIT -1 OFFSET ?", (offset,))
     rows = await cursor.fetchall()
     await cursor.close()
     await db.close()
@@ -22,11 +24,11 @@ async def fetch_rows_from_db(db_path, last_row_id=0):
 
 # Main function to fetch data from database and send it
 async def main():
-    last_row_id = 0
+    offset = 0
     while True:
-        rows = await fetch_rows_from_db("filtered_gold.db", last_row_id)
+        rows = await fetch_rows_from_db("filtered_gold.db", offset)
         if rows:
-            last_row_id = rows[-1][0]  # assuming first column is the id
+            offset += len(rows)
             for row in rows:
                 msg = str(row)  # convert row data to string
                 await send_more(chat_id, msg)
