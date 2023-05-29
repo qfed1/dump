b8b361936d27ec002f118f9b6a1919a76139aa7a
evan badding <evanbadding@gmail.com>
2023-05-29 11:45:24 -0600

    col

--- leanfancy.py
+++ leanfancy.py
@@ -24,11 +24,7 @@ cursor = conn.cursor()
 job_warnings = defaultdict(int)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Hi! Timer is set for 3 seconds")
-
-    chat_id = update.effective_message.chat_id
-    context.job_queue.run_repeating(alarm, 3, context={'chat_id': chat_id})
-
+    await update.message.reply_text("Hi! Use /set <seconds> to set a timer")
 
 async def fetch_message_after_wait(offset):
     await asyncio.sleep(5)
@@ -120,6 +116,24 @@ def remove_job_if_exists(name: str, context: ContextTypes.DEFAULT_TYPE) -> bool:
         job.schedule_removal()
     return True
 
+async def set_timer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    chat_id = update.effective_message.chat_id
+    try:
+        due = float(context.args[0])
+        if due < 0:
+            await update.effective_message.reply_text("Sorry we can not go back to future!")
+            return
+
+        job_removed = remove_job_if_exists(str(chat_id), context)
+        context.job_queue.run_repeating(alarm, due, chat_id=chat_id, name=str(chat_id), data=due)
+
+        text = "Timer successfully set!"
+        if job_removed:
+            text += " Old one was removed."
+        await update.effective_message.reply_text(text)
+
+    except (IndexError, ValueError):
+        await update.effective_message.reply_text("Usage: /set <seconds> DO NOT SET LOWER THAN 0.43 SECONDS UNDER NO CIRCUMSTANCES!!!")
 
 async def unset(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     chat_id = update.message.chat_id
@@ -131,6 +145,7 @@ def main() -> None:
     application = Application.builder().token(token).build()
 
     application.add_handler(CommandHandler(["start", "help"], start))
+    application.add_handler(CommandHandler("set", set_timer))
     application.add_handler(CommandHandler("unset", unset))
 
     application.run_polling()
