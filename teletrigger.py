040755cde77e40f388003ba92ab117960085eefb
evan badding <evanbadding@gmail.com>
2023-05-29 14:18:58 -0600

    sql

--- teletrigger.py
+++ teletrigger.py
@@ -1,10 +1,9 @@
 import asyncio
 import aiosqlite
 from telegram.ext import ApplicationBuilder
-from keys import token
+from keys import token as token1
 from keys import chatidmaster
-
-bot_token = token
+bot_token = token1
 chat_id = chatidmaster
 
 # using ApplicationBuilder
@@ -13,25 +12,43 @@ async def send_more(chat, msg):
     await application.bot.sendMessage(chat_id=chat, text=msg)
 
 # Function to retrieve rows from database
-async def fetch_rows_from_db(db_path, offset=0):
+async def fetch_rows_from_db(db_path):
     db = await aiosqlite.connect(db_path)
     cursor = await db.cursor()
-    await cursor.execute("SELECT * FROM gold LIMIT -1 OFFSET ?", (offset,))
+    await cursor.execute("SELECT * FROM filtered_messages")
     rows = await cursor.fetchall()
     await cursor.close()
     await db.close()
     return rows
 
+# Function to check if a message is in the sent_messages database
+async def is_sent(db_path, msg):
+    db = await aiosqlite.connect(db_path)
+    cursor = await db.cursor()
+    await cursor.execute("SELECT * FROM messages WHERE message = ?", (msg,))
+    row = await cursor.fetchone()
+    await cursor.close()
+    await db.close()
+    return row is not None
+
+# Function to add a message to the sent_messages database
+async def mark_as_sent(db_path, msg):
+    db = await aiosqlite.connect(db_path)
+    cursor = await db.cursor()
+    await cursor.execute("INSERT INTO messages (message) VALUES (?)", (msg,))
+    await db.commit()
+    await cursor.close()
+    await db.close()
+
 # Main function to fetch data from database and send it
 async def main():
-    offset = 0
     while True:
-        rows = await fetch_rows_from_db("filtered_gold.db", offset)
-        if rows:
-            offset += len(rows)
-            for row in rows:
-                msg = str(row)  # convert row data to string
+        rows = await fetch_rows_from_db("filtered_gold.db")
+        for row in rows:
+            msg = str(row)  # convert row data to string
+            if not await is_sent("sent_messages.db", msg):
                 await send_more(chat_id, msg)
+                await mark_as_sent("sent_messages.db", msg)
         await asyncio.sleep(5)  # wait for 5 seconds before polling again
 
 # Run the main function
