3258b896aadc39ebbe8b19b6df0f54711cf92d80
evan badding <evanbadding@gmail.com>
2023-06-04 13:14:07 -0600

    cool

--- contractdl.py
+++ contractdl.py
@@ -9,15 +9,39 @@ def get_contract_source(address):
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        contract_source_lines = soup.select('div.ace_content > div.ace_layer.ace_marker-layer > div.ace_line_group')
 
-        # Join the text of each line into one string
-        contract_source = '\n'.join(line.text for line in contract_source_lines)
+        # The HTML path provided
+        path = [
+            "html#html",
+            "body#body",
+            "main#content",
+            "section#ContentPlaceHolder1_divSummary",
+            "div#pills-tabContent",
+            "div#contracts",
+            "div.card.p-5.mb-3",
+            "div#code",
+            "div#dividcode",
+            "div.mb-4",
+            "pre#editor",
+            "div.ace_scroller",
+            "div.ace_content"
+        ]
 
-        return contract_source
+        # For each tag in the path, select it, and if there are multiple matches, take the first one
+        element = soup
+        for tag in path:
+            element = element.select_one(tag)
+            if element is None:
+                break
+
+        # Assuming that the last element is the one containing the contract source
+        if element is not None:
+            contract_source = element.text
+            return contract_source
     else:
         return None
 
 # Example usage:
 address = '0x36a17fbd22fb6b77f55ab797869700b663b026b6'
 print(get_contract_source(address))
+
