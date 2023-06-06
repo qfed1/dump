6dd195f7173fd17a1c8d8ac33f07ea159abba9d7
evan badding <evanbadding@gmail.com>
2023-06-06 01:56:55 -0600

    cool

--- contractdl.py
+++ contractdl.py
@@ -1,7 +1,5 @@
 import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 import pyperclip
 
@@ -14,11 +12,8 @@ def get_contract_source(address):
     url = f'https://etherscan.io/address/{address}#code'
     driver.get(url)
 
-    # Wait for Cloudflare DDoS protection to resolve (or any other element that is present after page load)
-    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'your_element_id')))
-
-    # Wait for the page to load
-    time.sleep(2)
+    # Wait for the user to manually verify that the page is ready
+    input("Press Enter after the Cloudflare check has completed...")
 
     # Find the button and click it
     button = driver.find_element(By.CSS_SELECTOR, "a.js-clipboard.btn.btn-sm.btn-icon.btn-secondary.me-1")
