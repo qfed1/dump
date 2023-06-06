f85d0bb7c7931116369d29a2ff8091711297ed93
evan badding <evanbadding@gmail.com>
2023-06-05 21:15:24 -0600

    cool

--- contractdl.py
+++ contractdl.py
@@ -1,14 +1,15 @@
-
-
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 import pyperclip
+import subprocess
 
 def get_contract_source(address):
-    # You need to have a webdriver downloaded and provide its path here
-    # You can download a webdriver from: https://sites.google.com/a/chromium.org/chromedriver/downloads
-    driver = webdriver.Chrome('/path/to/chromedriver')
+    # Setup webdriver
+    webdriver_service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=webdriver_service)
 
     url = f'https://etherscan.io/address/{address}#code'
     driver.get(url)
@@ -16,7 +17,7 @@ def get_contract_source(address):
     time.sleep(2)  # Wait for page to load
 
     # Find the button and click it
-    button = driver.find_element(By.XPATH, "//a[contains(@class,'js-clipboard')]")
+    button = driver.find_element(By.CSS_SELECTOR, "a.js-clipboard.btn.btn-sm.btn-icon.btn-secondary.me-1")
     button.click()
 
     time.sleep(2)  # Wait for the clipboard to get the text
@@ -26,9 +27,17 @@ def get_contract_source(address):
 
     driver.quit()
 
-    return contract_source
+    # Save the contract source to a .sol file
+    filename = f'{address}.sol'
+    with open(filename, 'w') as f:
+        f.write(contract_source)
+
+    # Run Slither analysis on the file
+    sol_filename = f'{filename}.sol'
+    slither_path = "/root/Desktop/dump/slither"
+    result = subprocess.run([slither_path, sol_filename], stdout=subprocess.PIPE)
+    print(result.stdout.decode('utf-8'))
 
 # Example usage:
 address = '0x36a17fbd22fb6b77f55ab797869700b663b026b6'
-print(get_contract_source(address))
-
+get_contract_source(address)
