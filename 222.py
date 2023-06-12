a047e9b38f04ec0776babcb5a6dc5fc456b3aa6c
evan badding <evanbadding@gmail.com>
2023-06-12 01:02:25 -0600

    cool

--- 222.py
+++ 222.py
@@ -1,58 +1,41 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
 import pyperclip
-import aiosqlite
-import asyncio
 
 def get_contract_source(address):
     # Setup webdriver
-    webdriver_service = Service('/usr/local/bin/chromedriver')
-    driver = webdriver.Chrome(service=webdriver_service)
+    options = webdriver.ChromeOptions()
+    options.add_argument('--disable-extensions')
+    driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', options=options)
 
     url = f'https://etherscan.io/address/{address}#code'
     driver.get(url)
 
-    time.sleep(2)  # Wait for page to load
+    # Wait for one URL change
+    old_url = driver.current_url
+    while old_url == driver.current_url:
+        time.sleep(0.5)  # Prevent CPU hogging
 
     # Find the button and click it
     button = driver.find_element(By.CSS_SELECTOR, "a.js-clipboard.btn.btn-sm.btn-icon.btn-secondary.me-1")
     button.click()
 
-    time.sleep(2)  # Wait for the clipboard to get the text
+    # Wait for the clipboard to get the text
+    time.sleep(2)
 
     # Get text from clipboard
     contract_source = pyperclip.paste()
 
     driver.quit()
 
-    return contract_source
+    # Save the contract source to a .sol file
+    filename = f'{address}.sol'
+    with open(filename, 'w') as f:
+        f.write(contract_source)
 
-async def main():
-    db_path = './filtered_messages.db'  # The path to the db file
+    print(f'Successfully saved the contract to {filename}')
 
-    # Connect to the SQLite database
-    async with aiosqlite.connect(db_path) as db:
-        db.row_factory = aiosqlite.Row  # This enables column access by name: row['column_name'] 
-
-        while True:  # Infinite loop
-            # Get the cursor
-            async with db.cursor() as cursor:
-                # Execute the SQL command
-                await cursor.execute('SELECT eth_address FROM filtered_messages')
-
-                # Fetch all rows
-                rows = await cursor.fetchall()
-
-                for row in rows:
-                    # Run the get_contract_source function in a separate thread
-                    loop = asyncio.get_running_loop()
-                    contract_source = await loop.run_in_executor(None, get_contract_source, row['eth_address'])
-                    print(contract_source)
-
-            # Sleep for a while before the next loop iteration
-            await asyncio.sleep(5)  # Adjust the sleep duration as needed
-
-# Run the main function
-asyncio.run(main())
+# Example usage:
+address = '0x36a17fbd22fb6b77f55ab797869700b663b026b6'
+get_contract_source(address)
