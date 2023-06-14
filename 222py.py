924e3bbbcdb50a35ccb5e5971fe8dff4cf5b779e
evan badding <evanbadding@gmail.com>
2023-06-14 01:22:20 -0600

    cool

--- 222py.py
+++ 222py.py
@@ -1,39 +1,32 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+import undetected_chromedriver.v2 as uc
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 import time
 import pyperclip
 
 def get_contract_source(address):
-    # Setup webdriver
-    webdriver_service = Service(ChromeDriverManager().install())
+    options = uc.ChromeOptions()
     
-    chrome_options = Options()
-    #chrome_options.add_argument("--headless")  # Ensure GUI is off
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+    # You might want to comment these lines if you don't run on a headless Linux server
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    with uc.Chrome(options=options) as driver:
+        url = f'https://etherscan.io/address/{address}#code'
+        driver.get(url)
     
-    driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
-
-    url = f'https://etherscan.io/address/{address}#code'
-    driver.get(url)
-
-    # Pause script and wait for user to press Enter
-    input("Press Enter to continue...")
-
-    # Find the button and click it
-    button = driver.find_element(By.CSS_SELECTOR, "a.js-clipboard.btn.btn-sm.btn-icon.btn-secondary.me-1")
-    button.click()
-
-    time.sleep(2)  # Wait for the clipboard to get the text
-
-    # Get text from clipboard
-    contract_source = pyperclip.paste()
-
-    driver.quit()
-
+        # Pause script and wait for user to press Enter
+        input("Press Enter to continue...")
+    
+        # Find the button and click it
+        button = driver.find_element(By.CSS_SELECTOR, "a.js-clipboard.btn.btn-sm.btn-icon.btn-secondary.me-1")
+        button.click()
+    
+        time.sleep(2)  # Wait for the clipboard to get the text
+    
+        # Get text from clipboard
+        contract_source = pyperclip.paste()
+        
     return contract_source
 
 # Example usage:
