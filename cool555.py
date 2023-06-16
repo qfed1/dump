88756c443e0cb27bb4888421548b8cd1d4366c6c
evan badding <evanbadding@gmail.com>
2023-06-16 02:37:36 -0600

    coll

--- cool555.py
+++ cool555.py
@@ -14,8 +14,10 @@ command = ["slither", "0xe88e3057Fa90C89CFF2B23c6Ce534F3C227D52F8d", "--print", 
 try:
     result = subprocess.run(command, check=True, capture_output=True, text=True)
 except subprocess.CalledProcessError as e:
-    print(f"An error occurred while running the command: {e}")
+    print(f"An error occurred while running the command: {e}\nError message:\n{e.stderr}")
     exit(1)
+
 
 # If the command completed successfully, print the command's output.
 print(result.stdout)
+
